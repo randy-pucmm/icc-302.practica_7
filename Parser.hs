529a5dfef5b1f4dd36d4d0056b5804f5b56d888d
Hugo Fernando Concepción López <158593180+VaQTaQ@users.noreply.github.com>
2026-03-17 09:08:36 -0400

    Add price parsing functions to Parser module
    hola

--- Parser.hs
+++ Parser.hs
@@ -1,1 +1,19 @@
 module Parser where
+
+parsePrice :: String -> Either String Double
+parsePrice s
+  | null s = Left "Contenido Vacio"
+  | otherwise = parse
+  where
+    xs = reads s
+    parse = case xs of
+      [(n, "")] -> Right n
+      _ -> Left "Error al parsear"
+
+parsePrices :: [String] -> Either String [Double]
+parsePrices xs = sequence (fmap parsePrice xs)
+
+pairProducts :: [String] -> [Double] -> Either String [(String, Double)]
+pairProducts products prices
+    | length products == length prices = Right (zip products prices)
+    | otherwise = Left "cantidad de productos y precios no coincide"
